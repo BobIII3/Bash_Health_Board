8f550aff32d5e11772447b3f82f85d23ffa2d40d
Theophile Trollet <trollettheophile@gmail.com>
2026-01-02 19:05:49 +0100

    Cleanning

--- dash_board.sh
+++ dash_board.sh
@@ -1,37 +1,55 @@
 #!/usr/bin/env bash
 
+# ---------------- FUNCTION ----------------
+
+get_date () {
+    $(date) 
+}
+
+get_time () {
+    $(uptime -p | sed 's/up //')  
+}
+    
+get_cpu_usage () {
+    
+    CHARGE_1MIN=$(uptime | awk -F'load average:' '{ print $2 }' | cut -d',' -f1)
+    NB_COEURS=$(nproc)
+
+    UTILISATION=$(echo "scale=3; ($CHARGE_1MIN / $NB_COEURS) * 100" | bc)
+}
+
+get_ram () {
+    (free -h | grep Mem | awk '{print $3 " / " $2}')
+}
+
+get_temp () {
+    
+    (cat /sys/class/thermal/thermal_zone*/temp | awk '{sum += $1; n++} END {if (n > 0) printf "%.1f", (sum / n)/1000 ; }' )
+}
+
+get_batterie () {
+
+    ( upower -i $( upower -e | grep BAT) | grep percentage | awk '{ print $2 }' )
+}
+
+get_disque () {
+
+    (df -h | grep /dev/nvme0n1p7 | awk '{printf "%.1f", ($3 / $2)*100 }')
+}
+
+# ---------------- DISPLAY  ----------------    
+ 
 echo "            ***********************************                "
 echo "            *                                 *                "
 echo "            *           Dash Board            *                "
 echo "            *                                 *                "
 echo "            ***********************************                "
 
-
-echo "    Date : $(date)    "
-echo "    Uptime : $(uptime -p | sed 's/up //')  "
-
-
-# Récupère la charge de la dernière minute
-CHARGE_1MIN=$(uptime | awk -F'load average:' '{ print $2 }' | cut -d',' -f1)
-NB_COEURS=$(nproc)
-
-# Calcul du pourcentage (via 'bc' pour avoir des décimales)
-UTILISATION=$(echo "scale=3; ($CHARGE_1MIN / $NB_COEURS) * 100" | bc)
-
-echo "    Utilisation globale : $UTILISATION %"
-
-echo "    RAM : $(free -h | grep Mem | awk '{print $3 " / " $2}')"
-
-TEMP=$(cat /sys/class/thermal/thermal_zone*/temp | awk '{sum += $1; n++} END {if (n > 0) printf "%.1f", (sum / n)/1000 ; }' )
-
-echo "    La température est : $TEMP °C"
-
-BATTERIE=$( upower -i $( upower -e | grep BAT) | grep percentage | awk '{ print $2 }' )
-
-echo "    Batterie : $BATTERIE "
-
-
-MEMOIRE=$(df -h | grep /dev/nvme0n1p7 | awk '{printf "%.1f", ($3 / $2)*100 }')
-
-echo "    Memoire : $MEMOIRE % utilise"
+echo "    Date : $(get_date) " 
+echo "    Uptime : $(get_time) "
+echo "    Utilisation globale :$(get_cpu_usage) %"
+echo "    RAM : $(get_ram)"
+echo "    La température est : $(get_temp) °C"
+echo "    Batterie : $(get_batterie) "
+echo "    Memoire : $(get_disque) % utilise"
 
